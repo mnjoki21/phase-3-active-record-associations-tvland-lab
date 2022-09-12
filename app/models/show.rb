a3e3b7a38fdcfe8863f66dcab3156ac8748d32c3
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-12 11:01:54 +0300

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  belongs_to :networks
+    has_many :characters
+    has_many :actors, through: :characters
+  
+    def actors_list
+      self.actors.map { |actor| actor.full_name }
+    end
+  
+    def network
+      self.networks.call_letters
+    end
   
 end
